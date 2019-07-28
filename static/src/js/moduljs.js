4a7f12ba302a9b3bfdc8acf06f867e3d5f4615d5
sakitkepala <andikapriyotama@gmail.com>
2019-07-28 23:13:51 +0700

    Definisi widget button baru

--- static/src/js/moduljs.js
+++ static/src/js/moduljs.js
@@ -3,5 +3,7 @@ odoo.define('moduljs.moduljs', function (require) {
     'use-strict';
     console.log("Halo dari moduljs!");
 
-    // var sAnimations = require();
+    var sAnimations = require('website.content.snippets.animation');
+
+    sAnimations.registry.moduljsButton = sAnimations.Class.extend({});
 });
