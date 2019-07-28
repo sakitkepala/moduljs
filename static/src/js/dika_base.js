c1e30cb3c0ebcf905cecb9d1c238085387c27745
sakitkepala <andikapriyotama@gmail.com>
2019-07-28 22:00:46 +0700

    Modul JS awal

--- static/src/js/dika_base.js
+++
@@ -1,5 +0,0 @@
-// moduljs
-odoo.define('dika_base.dika_base', function (require) {
-    'use-strict';
-    console.log("Halo dari moduljs!");
-});
---
+++ static/src/js/moduljs.js
@@ -0,0 +1,7 @@
+// moduljs
+odoo.define('moduljs.moduljs', function (require) {
+    'use-strict';
+    console.log("Halo dari moduljs!");
+
+    // var sAnimations = require();
+});
